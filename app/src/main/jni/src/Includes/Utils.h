fe9eb76469bc3671e0d224ca5a31fdce3d713b1d
Maxmomaric029 <112379738+Maxmomaric029@users.noreply.github.com>
2026-03-12 21:45:49 -0400

    Add files via upload

--- app/src/main/jni/src/Includes/Utils.h
+++ app/src/main/jni/src/Includes/Utils.h
@@ -2,11 +2,12 @@
 #define UTILS_H
 
 #include <pthread.h>
-#include "src/KittyMemory/MemoryPatch.h"
 #include <android/log.h>
 #include <jni.h>
 #include <unistd.h>
-#include "../KittyMemory/KittyMemory.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef unsigned long DWORD;
 
@@ -15,15 +16,34 @@ const char* libName = "libil2cpp.so";
 
 DWORD getRealOffset(DWORD address);
 
+// Función manual para encontrar la base de la librería (Compatible con versiones antiguas)
+DWORD get_libBase(const char* libName) {
+    FILE *fp;
+    DWORD addr = 0;
+    char filename[32], buffer[1024];
+    snprintf(filename, sizeof(filename), "/proc/self/maps");
+    fp = fopen(filename, "rt");
+    if (fp != NULL) {
+        while (fgets(buffer, sizeof(buffer), fp)) {
+            if (strstr(buffer, libName)) {
+                addr = (uintptr_t) strtoul(buffer, NULL, 16);
+                break;
+            }
+        }
+        fclose(fp);
+    }
+    return addr;
+}
+
 DWORD getRealOffset(DWORD address) {
     if (libBase == 0) {
-        // Usar KittyMemory para encontrar la base de la librería de forma segura
-        KittyMemory::ProcMap map;
-        if (KittyMemory::getMemoryBaseMap(libName, map)) {
-            libBase = map.startAddress;
-            __android_log_print(ANDROID_LOG_INFO, "9b_bloodie", "libil2cpp found at: %lx", libBase);
+        libBase = get_libBase(libName);
+        if (libBase == 0) {
+             __android_log_print(ANDROID_LOG_ERROR, "9b_bloodie", "libil2cpp NOT FOUND! Retrying...");
+             // Intento secundario si la primera lectura falla (común en algunos dispositivos)
+             libBase = get_libBase("libil2cpp.so");
         } else {
-            __android_log_print(ANDROID_LOG_ERROR, "9b_bloodie", "libil2cpp NOT FOUND!");
+             __android_log_print(ANDROID_LOG_INFO, "9b_bloodie", "libil2cpp found at: %lx", libBase);
         }
     }
     return (libBase + address);
